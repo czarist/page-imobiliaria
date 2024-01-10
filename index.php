46cc997efbb2e92ce800f8985642d0f547fdd1cc
Cezar Trentin <55374787+czarist@users.noreply.github.com>
2024-01-10 04:36:56 -0300

    ajuste rota

--- index.php
+++ index.php
@@ -1,12 +1,19 @@
 <?php
-function getBaseUrl() {
-    $secure = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') || $_SERVER['SERVER_PORT'] == 443 ? true : false;
+function getBaseUrl()
+{
+    $secure = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') || $_SERVER['SERVER_PORT'] == 443;
     $protocol = $secure ? 'https://' : 'http://';
     $host = $_SERVER['HTTP_HOST'];
-    $baseUrl = $protocol . $host;
 
-    return $baseUrl;
+    $path = $_SERVER['PHP_SELF'];
+    $pathParts = explode('/', $path);
+    array_pop($pathParts);
+    $directory = implode('/', $pathParts);
+
+    $baseUrl = $protocol . $host . $directory . '/';
+    return rtrim($baseUrl, '/');
 }
+
 
 include_once 'layouts/header.php';
 include_once 'sections/section-1.php';
